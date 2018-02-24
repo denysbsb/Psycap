56e61195f2066f78d3f4f5cbad0d937a790a8430
Denys Fontenelle Araujo <stef.denys.utic@sebrae.com.br>
2018-02-24 17:04:35 -0300

    message start

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const app = express();
+
+const bodyParser = require('body-parser');
+var rp = require('request-promise');
+const pg = require('pg');
+
+const { Client } = require('pg');
+
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+app.get('/', function(req, res) {
+    res.send('Hello Psycap!');
+})
+
+app.get('/criaTableUsuarios', function(req, res){
+    client.connect();
+
+    client.query('create table usuarios (id bigint auto_increment, nome text, email text, telefone text, senha text);', (err, res) => {
+        console.log('Criar tabela', res);
+    });
+
+});
+
+app.get('/verTableUsuarios', function(req, res){
+    client.connect();
+
+    client.query(`SELECT * FROM usuarios;`, (err, res) => {
+        console.log('Seleciona tabela usuarios res', res);
+        console.log('Seleciona tabela usuarios resrows', res.rows);
+    });
+
+    client.end();
+});
+
+app.post('/inserirUsuario', function(request, response) {
+    
+    var nome = 'Denys'; 
+    var email = 'denys.design@gmail.com';
+    var telefone = '984413353';
+    var senha = '123456';
+
+    let query = 'INSERT INTO usuarios VALUES '+ '('+ nome +','+ email +','+ telefone +','+ senha +')';
+
+    client.connect();
+
+    client.query(query, (err, result) => {
+        console.log('result insert', result);
+    });
+
+    client.end();
+
+});
+
+var porta = process.env.PORT || 8080;
+app.listen(porta, () => console.log('Example app listening on port 3000!'))
